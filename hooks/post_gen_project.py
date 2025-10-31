80c4d836e908455aeca3e9d26ab50759b59e3c01
Stefan Langenbach <stefan@langenbach.online>
2025-10-31 15:09:59 +0100

    Fix hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,15 +2,15 @@
 
 from pathlib import Path
 
-ROOT_PATH = Path("{{ cookiecutter.module_name }}")
-WORKFLOW_PATH = ROOT_PATH.joinpath(".github/workflows")
+WORKFLOW_PATH = Path("./.github/workflows")
 
 
 def remove_files_and_dirs(is_deployed_to_pypi: bool) -> None:
     """Remove files and directories."""
     files_and_dirs_to_remove: list[Path] = []
 
-    if not is_deployed_to_pypi:
+    if is_deployed_to_pypi is False:
+        print("Correct path")
         files_and_dirs_to_remove.append(WORKFLOW_PATH.joinpath("release.yml"))
 
     for obj in files_and_dirs_to_remove:
@@ -21,5 +21,5 @@ def remove_files_and_dirs(is_deployed_to_pypi: bool) -> None:
 
 
 if __name__ == "__main__":
-    IS_DEPLOYED_TO_PYPI: bool = "{{ cookiecutter.deploy_to_pypi }}"  # type: ignore[invalid-assignment]
+    IS_DEPLOYED_TO_PYPI = True if "{{ cookiecutter.deploy_to_pypi }}" == "True" else False
     remove_files_and_dirs(is_deployed_to_pypi=IS_DEPLOYED_TO_PYPI)
