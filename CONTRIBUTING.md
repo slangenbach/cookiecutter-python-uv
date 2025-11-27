51e81a041bfe1f4013dc25b955ad43531b21660b
Stefan Langenbach <stefan@langenbach.online>
2025-11-27 17:52:53 +0100

    Automate releases (#25)

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,4 +5,5 @@
 1. Add your contribution
 1. Run linters: `task lint`
 1. Run tests: `task test`
+1. Bump the project version: `task bump-version BUMP=<MAJOR|MINOR|PATCH>`
 1. Commit, push and create a merge request
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,13 +5,15 @@ from pathlib import Path
 WORKFLOW_PATH = Path("./.github/workflows")
 
 
-def remove_files_and_dirs(is_deployed_to_pypi: bool) -> None:
+def remove_files_and_dirs(is_deployed_to_pypi: bool, is_deployed_to_gh: bool) -> None:
     """Remove files and directories."""
     files_and_dirs_to_remove: list[Path] = []
 
     if is_deployed_to_pypi is False:
-        print("Correct path")
-        files_and_dirs_to_remove.append(WORKFLOW_PATH.joinpath("release.yml"))
+        files_and_dirs_to_remove.append(WORKFLOW_PATH / "release_to_pypi.yml")
+
+    if is_deployed_to_gh is False:
+        files_and_dirs_to_remove.append(WORKFLOW_PATH / "release_to_gh.yml")
 
     for obj in files_and_dirs_to_remove:
         if obj.is_dir():
@@ -22,4 +24,7 @@ def remove_files_and_dirs(is_deployed_to_pypi: bool) -> None:
 
 if __name__ == "__main__":
     IS_DEPLOYED_TO_PYPI = True if "{{ cookiecutter.deploy_to_pypi }}" == "True" else False
-    remove_files_and_dirs(is_deployed_to_pypi=IS_DEPLOYED_TO_PYPI)
+    IS_DEPLOYED_TO_GH = True if "{{ cookiecutter.deploy_to_gh }}" == "True" else False
+    remove_files_and_dirs(
+        is_deployed_to_pypi=IS_DEPLOYED_TO_PYPI, is_deployed_to_gh=IS_DEPLOYED_TO_GH
+    )
